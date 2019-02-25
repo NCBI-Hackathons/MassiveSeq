76a718482c04139f3b8a4083bae56a01687f0bb1
Doug Franz <khaverim7@gmail.com>
2019-02-25 16:29:54 -0500

    prep input script ready

--- prep_input/prep.py
+++ prep_input/prep.py
@@ -1,14 +1,19 @@
-import subprocess
+import os
+import sys
+
 
 # we are fetching files associated with archives with fastq-dump  (NCBI)
 # need to know if they are SINGLE or PAIRED first
+i=1
 with open('SraRunTable_noheader.txt') as f:    
     for line in f:
+        srr = line.find('SRR')
+        srrid = line[srr:srr+10]
+        print i, srrid
         if "SINGLE" in line:
-            print("sin")
-            subprocess.run(["ls", "-l"])
+            os.system('./fastq-dump '+srrid)
         elif "PAIRED" in line:
-            print("pai")
-
+            os.system('./fastq-dump -I --split-files '+srrid)
+        i += 1
 
 #/var/www/html/prep_input/sratoolkit.2.9.4-ubuntu64/bin/fastq-dump
